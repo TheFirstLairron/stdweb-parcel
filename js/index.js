4ae6ae907e6d84a8b72a10572e02d9c3272f40dd
TheFirstLairron <zrwall@hotmail.com>
2018-02-27 17:58:10 -0800

    Tried some other things

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 import Rust from '../Cargo.toml';
 
 let person = Rust.make_person("test_name", 1);
+console.log(Rust.option_test("some"));
+console.log(Rust.option_test("none"));
 document.getElementById("name").innerText = person.name;
 document.getElementById("age").innerText = person.age;
+
+// Threads are not available
+// Rust.threads();
--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,13 @@
 
 #[macro_use]
 extern crate stdweb;
-
 #[macro_use]
 extern crate serde_derive;
-
 extern crate serde;
 
 use stdweb::{js_deserializable, js_export, js_serializable};
+
+use std::thread;
 
 #[derive(Serialize, Deserialize)]
 struct Person {
@@ -22,4 +22,22 @@ js_deserializable!(Person);
 #[js_export]
 fn make_person(name: String, age: i32) -> Person {
     Person { name, age }
+}
+
+#[js_export]
+fn option_test(value: String) -> Option<String> {
+    if value == "some" {
+        Some(value)
+    } else {
+        None
+    }
+}
+
+#[js_export]
+fn threads() {
+    let handle = thread::spawn(move || {
+        
+    });
+
+    handle.join();
 }
