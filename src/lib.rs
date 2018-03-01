d9b68b678941d2d7973797f7ee2ab993d504332c
TheFirstLairron <zrwall@hotmail.com>
2018-03-01 12:51:20 -0800

    updated check to use the correct target

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,6 @@ extern crate serde;
 
 use stdweb::{js_deserializable, js_export, js_serializable};
 
-use std::thread;
-
 #[derive(Serialize, Deserialize)]
 struct Person {
     name: String,
@@ -31,13 +29,4 @@ fn option_test(value: String) -> Option<String> {
     } else {
         None
     }
-}
-
-#[js_export]
-fn threads() {
-    let handle = thread::spawn(move || {
-        
-    });
-
-    handle.join();
 }
